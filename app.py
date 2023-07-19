5e0eb3fa72493f4a88afb61b65d17dacc478abc7
Lasagna-guy-develops <sebastianmontoyazuluaga@gmail.com>
2023-07-18 22:09:42 -0500

    basic flask

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, Request, Response
 from flask_restful import Resource, Api
 from methods  import getData
-from DbConnection import DBInsert, sql_query
+from DbConnection import DBInsert, sql_query, sql_edit
 import datetime
 
 x = datetime.datetime.now()
@@ -14,10 +14,10 @@ app = Flask(__name__)
 def data():
     if Request.method == "GET":
         content = Request.json
-        usersQuery = '''SELECT * FROM Coach WHERE id like "''' + content["id"] + '";'
+        usersQuery = '''SELECT * FROM Coach WHERE id like "''' + content["Id"] + '";'
         user = sql_query(usersQuery)
 
-        json = {'id': user["id"], 'name': user["name"], 'Bdate': user["Bdate"]}
+        json = {'id': user["Id"], 'name': user["name"], 'Bdate': user["Bdate"]}
 
         return Response(json, status=200, mimetype='application/json')
     if Request.method == "POST":
@@ -25,6 +25,13 @@ def data():
             content = Request.json
             PersonInsertQuery = '''INSERT INTO Person(firstname, Bdate) Values(%s,%s);'''
             DBInsert(PersonInsertQuery, (content["name"], content["Bdate"]))
+            return Response(status=200)
+        except:
+            return Response(status=420)
+    if Request.method == "PUT":
+        try:
+            content = Request.json
+            sql_query('''UPDATE Person SET firstname=%s, Bdate=%s WHERE Id = %s;''',(content["name"], content["Bdate"], content["Id"]))
             return Response(status=200)
         except:
             return Response(status=420)
