2bc05b94a1fdb36171e81589b8ae4e47fac5d6a7
Lasagna-guy-develops <sebastianmontoyazuluaga@gmail.com>
2023-07-19 00:35:30 -0500

    fix

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def data():
     print(request)
     if request.method == "GET":
         content = request.json
-        usersQuery = '''SELECT * FROM Person WHERE id like "''' + content["Id"] + '";'
+        usersQuery = '''SELECT * FROM Person WHERE Id like "''' + content["Id"] + '";'
         user = sql_query(usersQuery)
 
         json = {'id': user["Id"], 'name': user["name"], 'email': user["email"]}
@@ -24,7 +24,7 @@ def data():
             print(request)
             print(request.json)
             content = request.json
-            PersonInsertQuery = '''INSERT INTO Person(firstname, Bdate) Values(%s,%s);'''
+            PersonInsertQuery = '''INSERT INTO Person(name, email) Values(%s,%s);'''
             DBInsert(PersonInsertQuery, (content["name"], content["email"]))
             return Response(status=200)
         except Exception as e:
@@ -33,7 +33,7 @@ def data():
     if request.method == "PUT":
         try:
             content = request.json
-            sql_query('''UPDATE Person SET firstname=%s, email=%s WHERE Id = %s;''',(content["name"], content["email"], content["Id"]))
+            sql_query('''UPDATE Person SET name=%s, email=%s WHERE Id = %s;''',(content["name"], content["email"], content["Id"]))
             return Response(status=200)
         except:
             return Response(status=420)
